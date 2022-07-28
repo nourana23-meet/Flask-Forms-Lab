66bf2b76d2d2e16be2c28496b6d18b583139444d
nourana23-meet <109900008+nourana23-meet@users.noreply.github.com>
2022-07-28 18:42:43 +0300

    fininshed

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -7,17 +7,30 @@ app = Flask(  # Create a flask app
 	static_folder='static'  # Name of directory for static files
 )
 
-
-username = "llo2ay"
+username = "noura"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["beb","Yonathan","Adan", "George", "Fouad", "Celina"]
+name = "yaso"
 
-
-@app.route('/')  # '/' for the default page
+@app.route('/',methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'POST':
+		if username == request.form['username'] and password == request.form['password']:
+			return redirect(url_for('Home'))
+		else:
+			return render_template('login.html')
+	return render_template('login.html')
 
+@app.route('/home')
+def Home():
+	return render_template('home.html',friends=facebook_friends)
+
+@app.route('/friend_exists/<string:name>',methods=['GET', 'POST'])
+def Friends(name):
+    if name in facebook_friends:
+    	boolean = 'TRUE'
+    	return render_template('friend_exists.html', n = name, b = boolean)
+        
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
